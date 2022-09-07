10fe0cccf36bb226d6ce7335342f6e9f33b0e297
seo0033 <tkrhk9511@gmail.com>
2022-09-07 14:49:40 +0900

    20220907

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+const DB = [
+  {
+    id: 1,
+    content: "menu01",
+    link: "/s1",
+    submenu: [
+      { content: "smenu01", link: "/" },
+      { content: "smenu02", link: "/" },
+      { content: "smenu03", link: "/" }
+    ]
+  },
+  {
+    id: 2,
+    content: "menu02",
+    link: "/s1",
+    submenu: [
+      { content: "smenu01", link: "/" },
+      { content: "smenu02", link: "/" },
+      { content: "smenu03", link: "/" }
+    ]
+  },
+  {
+    id: 3,
+    content: "menu03",
+    link: "/s1",
+    submenu: [
+      { content: "smenu01", link: "/" },
+      { content: "smenu02", link: "/" },
+      { content: "smenu03", link: "/" }
+    ]
+  },
+
+]
+
 function App() {
+  const [CC, setCC] = useState("");
+  // CC(ClassChage)내가 설정한거!
+  const [TG, setTG] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header>
+      <h1 className={CC} onClick={() => setCC('on')}>LOGO</h1>
+      <h2 className={`ggg ${TG ? 'ox' : ''}`}>Toggle Class</h2>
+
+      {/* <button onClick={() => setCC('on')}>class add</button> */}
+      <button onClick={() => setTG(!TG)}>class toggle</button>
+
+      <nav className='GNB'>
+        <ul>
+          {
+            DB.map((it, idx) => (<li key={idx}>
+              <a href="">{it.content}</a>
+
+              <ul className='smenu'>
+                {
+                  it.submenu.map((smenu, idx) =>
+                    <li key={idx}>
+                      <a href={smenu.link}>{smenu.content}</a>
+                    </li>)
+                }
+
+                {/* <li>
+                <a href=''>{it.submenu[0].content}</a>
+              </li> */}
+              </ul>
+            </li>))
+          }
+
+          {/* <li>{DB[0].name}</li> */}
+        </ul>
+      </nav>
+    </header>
   );
 }
 
 export default App;
+
